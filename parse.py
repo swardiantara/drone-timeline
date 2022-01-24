990cbe33b3b674be05801e0028cb48837f03ef81
swardiantara <swardyantara@gmail.com>
2022-01-24 14:59:46 +0700

    df067 2018/09 done

--- parse.py
+++ parse.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 import json
 
-path = r"E:\6025211018 - Swardiantara S\Drone Datasets\DJI_Matrice_600\df034\2018_June\mobile_iOS_backup\df034\Export"
+path = r"E:\6025211018 - Swardiantara S\Drone Datasets\DJI_Mavic_2\df067\mobile_iOS_backup\df067\Export"
 
 os.chdir(path)
 
